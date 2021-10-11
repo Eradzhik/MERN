108e9d402ca09f306e71d9f15a3b7353bdba3b60
Eradzhik <Eradzh0777@gmail.com>
2021-10-11 01:50:44 -0400

    More Form

--- React/MoreForms/moreforms/src/components/Form.js
+++ React/MoreForms/moreforms/src/components/Form.js
@@ -1,12 +1,69 @@
 import React, {useState} from 'react';
 
 
+
 const Form = (props) =>{
     const[firstName,setFirstName]=useState("")
     const[lastName,setLastName]=useState("")
     const[email,setEmail]=useState("")
     const[password,setPassword]=useState("")
     const[confirmPassword,setConfirmPassword]=useState("")
+    const[firstNameError,setFirstNameError]=useState("")
+    const[lastNameError,setLastNameError]=useState("")
+    const[emailError,setEmailError]=useState("")
+    const[passwordError,setPasswordError]=useState("")
+    const[confirmPasswordError,setConfirmPasswordError]=useState("")
+
+    const handleFirstName = (e)=>{
+        setFirstName (e.target.value);
+        if(e.target.value.length<2) {
+            setFirstNameError("Field must be at least two characters")
+        } else {
+            setFirstNameError("")
+        }
+    }
+
+    const handleLastName =e=>
+        {
+            setLastName(e.target.value)
+            if(e.target.value.length<2){
+                setLastNameError("Field must at least two characters")
+            } else {
+                setLastNameError("")
+            }
+                
+        }
+
+        const handleEmail = (e)=>{
+                setEmail(e.target.value)
+            if(e.target.value.length<5){
+                setEmailError("Field should be more than 5")
+            } else {
+                setEmailError("")
+            }
+        }
+
+        const handlePassword = (e)=>{
+            setPassword(e.target.value)
+        if(e.target.value.length<8){
+            setPasswordError("Field should be more than 8")
+        } else {
+            setPasswordError("")
+        }
+    }
+    const handleConfirmPassword = (e)=>{
+        setConfirmPassword(e.target.value)
+    if(e.target.value!==password){
+        setConfirmPasswordError("Password must match!")
+    } else {
+        setConfirmPasswordError("")
+    }
+}
+
+
+
+
+
 
 
     //test
@@ -16,24 +73,50 @@ const Form = (props) =>{
     <form>
         <div>
             <label>First Name</label>
-            <input type="text" value={firstName} onChange={e=>setFirstName(e.target.value)}></input>
+            <input type="text" value={firstName} onChange={handleFirstName}></input>
             <button onClick={e=>setFirstName("")}>Clear</button>
+            {
+            firstNameError?
+            <p>{firstNameError}</p>
+            : null
+            }
         </div>
         <div>
             <label>Last Name</label>
-            <input type="text" value={lastName} onChange={e=>setLastName(e.target.value)}></input>   
+            <input type="text" value={lastName} onChange={handleLastName}></input>   
+            {
+                lastNameError?
+                <p>{lastNameError}</p>
+                :null
+            }
         </div>
         <div>
             <label htmlFor="email">Email</label>
-            <input type="text" value={email} onChange={e=>setEmail(e.target.value)} id="email"/>
+            <input type="text" value={email} onChange={handleEmail} id="email"/>
+            {
+                emailError?
+                <p>{emailError}</p>
+                :null
+            }
         </div>
         <div>
             <label htmlFor="password">Password</label>
-            <input type="password" value={password} onChange={e=>setPassword(e.target.value)} id="password"/>
+            <input type="password" value={password} onChange={handlePassword} id="password"/>
+            {
+                passwordError?
+                <p>{passwordError}</p>
+                :null
+            }
+
         </div>
         <div>
             <label htmlFor="confirmPassword">Confirm Password</label>
-            <input type="password" value={confirmPassword} onChange={e=>setConfirmPassword(e.target.value)}  id="confirmPassword"/>
+            <input type="password" value={confirmPassword} onChange={handleConfirmPassword}  id="confirmPassword"/>
+            {
+                confirmPasswordError?
+                <p>{confirmPasswordError}</p>
+                :null
+            }
         </div>
     </form>
 
